3d9eb20a7437916c9d32ddad5e51786276f34d2b
Rory Murray <rory.christian.murray@gmail.com>
2019-08-13 21:56:33 -0400

    Build RestaurantService

--- crudyrestaurants/src/main/java/com/lambdaschool/crudyrestaurants/services/RestaurantService.java
+++ crudyrestaurants/src/main/java/com/lambdaschool/crudyrestaurants/services/RestaurantService.java
@@ -1,5 +1,21 @@
 package com.lambdaschool.crudyrestaurants.services;
 
+import com.lambdaschool.crudyrestaurants.model.Restaurant;
+
+import java.util.List;
+
 public interface RestaurantService
 {
+    List<Restaurant> findAll();
+
+    Restaurant findRestaurantById(long id);
+
+    Restaurant findRestaurantByName(String name);
+
+    void delete(long id);
+
+    Restaurant save(Restaurant restaurant);
+
+    Restaurant update(Restaurant restaurant, long id);
+
 }
